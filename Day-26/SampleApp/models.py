cb937128b16837ca5c4f9e5a18afa8147ae6c847
internship <rravikumar34@gmail.com>
2023-06-20 12:16:46 +0530

    Day-26 Updated

---
+++ Day-26/SampleApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+	sroll = models.CharField(max_length=15)
+	sname = models.CharField(max_length=30)
+	syear = models.CharField(max_length=5)
+	sbranch = models.CharField(max_length=50)
+	sage = models.IntegerField(default=18)
+
+	def __str__(self):
+		return self.sroll
---
+++ Day-26/SampleApp/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Student
+# Create your views here.
+
+def sa(request):
+	return HttpResponse("Good Morning")
+
+def printname(request,p):
+	return HttpResponse(f"Good Morning {p}")
+
+def stdtls(request,n,a):
+	return HttpResponse(f"Student Details<br>Name: {n}<br>Age: {a}")
+
+def empdtls(request,eage,epid,ename):
+	return HttpResponse(f"<h3>Employee Data</h3><h4>Employee Name: {ename}</h4><h4>Employee Id: {epid}</h4><h4>Employee Age: {eage}</h4>")
+
+def stprint(request,sid,stname):
+	return HttpResponse(f"<h3>Student Name: <span style='background-color:green;color:white'>{stname}</span></h3><h3>Student Id: <i style='background-color:yellow;color:blue'>{sid}</i></h3>")
+
+def eprint(request,ename):
+	return HttpResponse("<style>span{background-color:cadetblue;color:white}</style><h4>Employee Name: <span>%s</span></h4>"%(ename))
+
+def jprint(request,sname,y):
+	return HttpResponse(f"<script>alert('Hi welcome User {sname}')</script><h4>Welcome User {sname}</h4><h5>Age: {y}</h5>")
+
+def gy(request):
+	return HttpResponse("<html><head><title>Demo Html Page</title></head><body>Hi User</body></html>")
+
+def vb(request):
+	return render(request,'demo.html')
+
+def idt(request):
+	return render(request,'html/index.html')
+
+def dy(request):
+	return render(request,'html/sample.html')
+
+def btp(request):
+	return render(request,'html/home.html')
+
+def about(request):
+	return render(request,'html/about.html')
+
+def contact(request):
+	return render(request,'html/contact.html')
+
+def register(request):
+	if request.method == "POST":
+		r = request.POST['rn']
+		n = request.POST['nm']
+		e = request.POST['em']
+		age = request.POST['ag']
+		return render(request,'html/display.html',{'a':r,'b':n,'c':e,'d':age})
+	return render(request,'html/register.html')
+
+def index(request):
+	w = Student.objects.all()
+	return render(request,'html/dy.html',{'st':w})
+
+
+
+
